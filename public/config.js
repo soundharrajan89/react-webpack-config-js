2c6702efe62de2e252ab9f1bbcd849b7191319a8
soundharrajan89 <soundharrajan89@gmail.com>
2023-08-17 19:25:23 +0530

    env variables added through webpack with config.js file

--- public/config.js
+++ public/config.js
@@ -1,7 +1,2 @@
-// config.js
-const config = {
-    REACT_APP_NAME: 'your-secret-api-key',
-    // Add more sensitive variables here
-  };
-  
-  export default config;
+var REACT_APP_NAME = 'https://access2.sabic.com/logon/LogonPoint/index.html'
+var REACT_APP_NAME_ROUTER = 'https://access2.sabic.com/logon/LogonPoint/index.htmlddf'
